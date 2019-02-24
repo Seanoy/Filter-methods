521def053215c5ed413764492ab7cbcc63fa7543
Seanoy <413337211@qq.com>
2019-02-24 17:59:33 +0800

    update

--- 消抖滤波法/消抖滤波法/源.cpp
+++ 消抖滤波法/消抖滤波法/源.cpp
@@ -3,29 +3,20 @@ using namespace std;
 
 
 #define N 12
-char value;					//上次采样到的数据
-char get_val()
-{
-	char data = 0;
-	//采样数据相关程序
-	//...
-	
-	return data;
-}
-
-char filter() 
+char get_val();
+char filter()
 {
 	char count = 0;
-	char new_value;
-	new_value = get_val();//获取采样数据
-	while (value != new_value)
+	char value_now;
+	char value_before = get_val();	//获取上一个采样数据
+	while (value_now != value_before)
 	{
+		value_before = value_now;
 		count++;
 		if (count >= N)
-			return new_value;
+			return value_before;
 		//delay();//适当延时
-		new_value = get_val();//获取下一个采样数据
-
+		value_now = get_val();//获取下一个采样数据
 	}
-	return value;
+	return value_now;
 }
--- 限幅平均滤波法/限幅平均滤波法/源.cpp
+++ 限幅平均滤波法/限幅平均滤波法/源.cpp
@@ -5,26 +5,50 @@ using namespace std;
 限幅平均滤波法
 优点：对于偶然出现的脉冲性干扰，可消除由其引起的采样值偏差。
 *************************************************/
-
-#define A 10
-#define N 12
-
-char filter(char data[])
+#define E 10         //误差允许值
+#define N 12        //采样数据数目
+char value_init = 100;  //采样参考值
+char temp;
+char getval();
+char filter(void)
 {
-	char i, value, sum;
-	//data[N] = getval();
-	if ((data[N] - data[N - 1]) > A || (data[N - 1] - data[N]) > A)
+	char i, value_now, value_before, sum, count = 0;
+	//count:防止传感器出错而陷入死循环
+	char *buf = (char*)malloc(N * sizeof(char));
+	while (1)//直到取得有效值为止
 	{
-		data[N] = data[N - 1];
+		temp = getval();
+		if ((temp - value_init >E) || (temp - value_init <E))
+		{
+			temp = getval();
+			count++;
+				if (count == 5) {
+					buf[0] = value_init;
+					break;
+				}
+		}
+		else
+		{
+			buf[0] = temp;            //获取采样值用作上一次的值
+			break;
+		}
 	}
-	//else
-		//data[N] = newval;
+
+	for (i = 1; i < N; i++)     //过滤超出范围的值
+	{
+		value_now = getval();     //获取采样值用作上一次的值
+		if ((value_now - buf[i - 1]) > E || (buf[i - 1] - value_now) > E)
+		{
+			buf[i] = buf[i - 1];        //本次数据等于上一数据
+		}
+		else
+			buf[i] = value_now;           //本次数据有效
+	}
+
 	for (i = 0; i < N; i++)
 	{
-		data[i] = data[i + 1];
-		sum += data[i];
+		buf[i] = buf[i + 1];
+		sum += buf[i];
 	}
 	return (sum / N);
 }
-
-
